46e79b42a70d71df9931fd01358b9f48b3fd78cd
Viktor Lott <viktorlott@Viktors-MacBook-Pro.local>
2019-07-25 17:41:33 +0200

    commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// NjAzNjU3MTYyNTQzMjAyMzU3.XTixjg.APSAZEhKsvA9FC0E_IP1pp59PjI
+// https://discordapp.com/oauth2/authorize?client_id=603657162543202357&scope=bot&permissions=8
+// Admin ^
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  console.log("Servers:");
+  client.guilds.forEach(guild => {
+    console.log(" - " + guild.name);
+
+    // List all channels
+    guild.channels.forEach(channel => {
+      console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
+    });
+  });
+
+  const generalChannel = client.channels.get("603674539184095245");
+
+  generalChannel.send("Wassup retards");
+});
+
+client.on("message", msg => {
+  console.log(msg);
+  if (msg.content === "you are gay") {
+    msg.reply("no u");
+  }
+
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+});
+
+client.login("NjAzNjU3MTYyNTQzMjAyMzU3.XTixjg.APSAZEhKsvA9FC0E_IP1pp59PjI");
